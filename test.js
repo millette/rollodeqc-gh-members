014947bca40e1ac2145801c738d0a68031c19001
Robin Millette <robin@millette.info>
2016-05-16 20:54:01 -0400

    Fix silly bug in tests.

--- test.js
+++ test.js
@@ -7,5 +7,5 @@ test('sherbrooke', async t => {
   const results = await fn({ o: { location: 'sherbrooke' } })
   t.is(results[0].login, 'introlab')
   t.is(results[0].public_members.length, 4)
-  t.is(results[0].length, 27)
+  t.is(results.length, 27)
 })
