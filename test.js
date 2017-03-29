0ad6f673457660c9777455fb8783270af18ec5e4
Robin Millette <robin@millette.info>
2017-03-29 15:48:01 -0400

    Fix brittle tests.

--- test.js
+++ test.js
@@ -5,6 +5,6 @@ import fn from './'
 test('sherbrooke', async t => {
   const results = await fn({ o: { location: 'sherbrooke' } })
   t.is(results[0].login, 'introlab')
-  t.is(results[0].public_members.length, 5)
+  t.is(results[0].public_members.length, 6)
   t.is(results.length, 35)
 })
