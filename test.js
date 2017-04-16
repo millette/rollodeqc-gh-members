26c00e7654115ccc3d56a9f7413e852a326b47fd
Robin Millette <robin@millette.info>
2017-04-16 06:20:09 -0400

    Fix brittle tests.

--- test.js
+++ test.js
@@ -7,37 +7,5 @@ test('sherbrooke', async t => {
   const results = await fn({ o: { location: 'sherbrooke' } })
   t.is(results[0].login, 'introlab')
   t.is(results[0].public_members.length, 6)
-  t.is(results.length, 35)
+  t.is(results.length, 36)
 })
-
-/*
-test('sherbrooke', t => {
-  t.plan(3)
-  fn({ o: { location: 'sherbrooke' } })
-    .then((results) => {
-      console.log('results', results)
-      t.is(results[0].login, 'introlab')
-      t.is(results[0].public_members.length, 6)
-      t.is(results.length, 35)
-    })
-    .catch((err) => {
-      t.truthy(err)
-      console.log('ERR:', err)
-    })
-})
-
-test('joliette', t => {
-  t.plan(3)
-  fn({ o: { location: 'joliette' } })
-    .then((results) => {
-      console.log('results', results)
-      t.is(results[0].login, 'introlab')
-      t.is(results[0].public_members.length, 6)
-      t.is(results.length, 35)
-    })
-    .catch((err) => {
-      t.truthy(err)
-      console.log('ERR:', err)
-    })
-})
-*/
