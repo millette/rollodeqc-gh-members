2f128758b125e97670e4664cee53d1f8b53f48b1
Robin Millette <robin@millette.info>
2017-04-16 06:07:32 -0400

    Hmm, tests are failing...

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 'use strict'
 import test from 'ava'
+
 import fn from './'
 
 test('sherbrooke', async t => {
@@ -8,3 +9,35 @@ test('sherbrooke', async t => {
   t.is(results[0].public_members.length, 6)
   t.is(results.length, 35)
 })
+
+/*
+test('sherbrooke', t => {
+  t.plan(3)
+  fn({ o: { location: 'sherbrooke' } })
+    .then((results) => {
+      console.log('results', results)
+      t.is(results[0].login, 'introlab')
+      t.is(results[0].public_members.length, 6)
+      t.is(results.length, 35)
+    })
+    .catch((err) => {
+      t.truthy(err)
+      console.log('ERR:', err)
+    })
+})
+
+test('joliette', t => {
+  t.plan(3)
+  fn({ o: { location: 'joliette' } })
+    .then((results) => {
+      console.log('results', results)
+      t.is(results[0].login, 'introlab')
+      t.is(results[0].public_members.length, 6)
+      t.is(results.length, 35)
+    })
+    .catch((err) => {
+      t.truthy(err)
+      console.log('ERR:', err)
+    })
+})
+*/
