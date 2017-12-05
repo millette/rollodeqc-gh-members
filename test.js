4059a7c235f49ba74bb4c9b05449b3ffa253d9e7
Robin Millette <robin@millette.info>
2017-12-05 14:09:09 -0500

    Fix tests.

--- test.js
+++ test.js
@@ -7,5 +7,5 @@ test('sherbrooke', async t => {
   const results = await fn({ o: { location: 'sherbrooke' } })
   t.is(results[0].login, 'introlab')
   t.is(results[0].public_members.length, 6)
-  t.is(results.length, 36)
+  t.is(results.length, 42)
 })
