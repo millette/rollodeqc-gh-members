436b7f1ecff111f6f30d70bc6cd7a3102d822b0a
Robin Millette <robin@millette.info>
2019-07-12 17:32:58 -0400

    Add timeout to allow tests to finish.

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ import test from 'ava'
 import fn from './'
 
 test('sherbrooke', async t => {
+  t.timeout(100000)
   const results = await fn({ o: { location: 'sherbrooke' } })
   t.is(results[0].login, 'introlab')
   t.is(results[0].public_members.length, 6)
